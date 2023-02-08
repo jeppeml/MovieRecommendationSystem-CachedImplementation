16adb2d4af73e7efcfab3d505a05d46b461d2636
brunstighanhval <mlkaer1@hotmail.com>
2023-02-08 09:54:52 +0100

    æoip

--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -1,5 +1,7 @@
 package dk.easv;
 
+import dk.easv.presentation.controller.LogInController;
+import dk.easv.presentation.model.AppModel;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,16 +12,25 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("presentation/view/LogIn.fxml"));
-        primaryStage.setTitle("Movie Recommendation System 0.01 Beta");
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("presentation/view/LogIn.fxml"));
+        Parent root = loader.load();
+        
+        primaryStage.setTitle("Movie Recommendation System 1.1");
         // primaryStage.setFullScreen(true);
         primaryStage.setScene(new Scene(root));
-        primaryStage.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        primaryStage.getScene().getStylesheets().add(getClass().getResource("/CSS/Login.css").toExternalForm());
+        primaryStage.setMaxHeight(620);
+        primaryStage.setMaxWidth(700);
+        primaryStage.setMinHeight(620);
+        primaryStage.setMinWidth(700);
+        primaryStage.setResizable(false);
         primaryStage.show();
+
+
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch(args);
     }
 }
