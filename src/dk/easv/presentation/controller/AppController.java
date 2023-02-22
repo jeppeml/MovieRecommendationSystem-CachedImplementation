ee12facba800a6c6ccb04701075ef55405d0219c
brunstighanhval <mlkaer1@hotmail.com>
2023-02-22 10:22:05 +0100

    bjk

--- src/dk/easv/presentation/controller/AppController.java
+++ src/dk/easv/presentation/controller/AppController.java
@@ -57,7 +57,6 @@ public class AppController implements Initializable {
     public void setModel(AppModel model) throws FileNotFoundException {
         this.model = model;
         showUserName();
-        greaterAndLessThan();
         getTopAverageRatedMovies=model.getTopAverageRatedMovies();
         getTopAverageRatedMoviesUserDidNotSee=model.getTopAverageRatedMoviesUserDidNotSee();
         getTopMoviesFromSimilarPeople=model.getTopMoviesFromSimilarPeople();
@@ -65,26 +64,7 @@ public class AppController implements Initializable {
         setUpGribPanes("getTopAverageRatedMoviesUserDidNotSee",1);
         setUpGribPanes("getTopMoviesFromSimilarPeople",2);
     }
-    public void greaterAndLessThan() throws FileNotFoundException {
 
-
-
-      //  ImageView myImageView;
-      //  Image myImage = new Image(getClass().getResourceAsStream("Resources/Pictures/GreaterThan.png"));
-     /*   ImageView imageView= new ImageView(picture);
-        imageView.setFitWidth(75);
-        imageView.setFitHeight(100);
-        VBox vbox=new VBox();
-        vbox.setMinSize(200,200);
-        vbox.setMaxSize(200,200);
-        vbox.getChildren().add(imageView);
-
-      */
-
-    }
-    public void displayImage(){
-
-    }
 
     public void setUpGribPanes(String listName, int tilePane) throws FileNotFoundException {
 
@@ -121,24 +101,6 @@ public class AppController implements Initializable {
                 movieTitle = new javafx.scene.control.Label(line);
                 picture =  new Image(new FileInputStream(("Resources/Pictures/Flower"+r2.nextInt(13)+".jpg")));
             }
-          /*  else if
-            {
-                line=changeLineInAString(getTopMoviesFromSimilarPeople.get(i).getTitle());
-                movieTitle = new javafx.scene.control.Label(line);
-                picture =  new Image(new FileInputStream(("Resources/Pictures/GreaterThan.png")));
-            }
-            else
-            {
-                line=changeLineInAString(getTopMoviesFromSimilarPeople.get(i).getTitle());
-                movieTitle = new javafx.scene.control.Label(line);
-                picture =  new Image(new FileInputStream(("Resources/Pictures/Less than.png")));
-            }
-
-           */
-
-
-
-
             ImageView imageView= new ImageView(picture);
             imageView.setFitWidth(150);
             imageView.setFitHeight(200);
